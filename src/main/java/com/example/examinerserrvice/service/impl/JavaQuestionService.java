1588cac5126f1bf78e48c4c685aca26bfa7996fa
Rijaia <123201938+Rijaia@users.noreply.github.com>
2023-07-12 16:30:41 +0300

    Revert "Course work"

--- src/main/java/com/example/examinerserrvice/service/impl/JavaQuestionService.java
+++ src/main/java/com/example/examinerserrvice/service/impl/JavaQuestionService.java
@@ -6,15 +6,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 
 @Service
 public class JavaQuestionService implements QuestionService {
     private final List<Question> questions = new ArrayList<>();
-   private final Random random = new Random();
-
 
     @PostConstruct
     public void setup() {
@@ -36,24 +32,19 @@ public class JavaQuestionService implements QuestionService {
         questions.add(question);
         return question;
     }
-    @Override
-    public Question remove(String question, String answer) {
-        return remove(new Question(question, answer));
-    }
+
     @Override
     public Question remove(Question question) {
-        boolean isRemove = questions.remove(question);
-        return isRemove ? question : null;
+        return null;
     }
 
     @Override
-    public Collection<Question> getAll() {
-        return new ArrayList<>(questions);
+    public Question getAll() {
+        return null;
     }
 
     @Override
     public Question getRandomQuestion() {
-        int randomIndex = random.nextInt(questions.size());
-        return questions.get(randomIndex);
+        return null;
     }
 }
